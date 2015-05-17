a9a6d90b84748d9a4e229cfc1a5770745d53ff85
samarurlajt <samarurlaj@gmail.com>
2015-05-17 14:02:43 +0530

    Provider added

---
+++ src/SimpleAdminServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Samarulrajt\SimpleAdmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleAdminServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+            $this->loadViewsFrom(__DIR__.'/views', 'simpleAdmin');
+            $this->publishes([
+    __DIR__.'/views' => base_path('resources/views/samarulrajt/simpleAdmin'),
+]);
+            
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		
+            include __DIR__.'/routes.php';
+            $this->app->make('Samarulrajt\SimpleAdmin\SimpleAdminController');
+	}
+
+}
